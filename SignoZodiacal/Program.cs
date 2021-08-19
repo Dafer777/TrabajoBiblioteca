21cd4ec9ae10d3827f8297ba188c35d47e2a96c4
50370 <50370@DESKTOP-2S98IHC>
2021-08-19 08:45:00 -0600

    Agregar archivos de proyecto.

---
+++ SignoZodiacal/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TrabajoBiblioteca;
+
+namespace SignoZodiacal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1.SignosZodiacales();
+            Console.ReadKey();
+        }
+    }
+}
